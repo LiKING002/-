a50e13d7e382cdbe92e160a05b38f1f48e446b9a
KIN_002 <67863453+LiKING002@users.noreply.github.com>
2020-09-10 17:58:00 +0800

    Add files via upload

---
+++ gmail.php
@@ -0,0 +1,60 @@
+
+<?php
+function sendMail($to,$title,$content){
+ 
+    require_once("phpmailer/class.phpmailer.php"); 
+    require_once("phpmailer/class.smtp.php");
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+ 
+    $mail->SMTPAuth=true;
+ 
+    //链接邮箱域名的服务器地址
+    $mail->Host = 'mail.google.com'';
+ 
+    $mail->SMTPOptions = array(
+	    'ssl' => array(
+	        'verify_peer' => false,
+	        'verify_peer_name' => false,
+	        'allow_self_signed' => true
+	    )
+	);
+ 
+    $mail->SMTPSecure = 'ssl';
+ 
+    // 可选465或587
+    $mail->Port = 465;
+ 
+    $mail->Hostname = 'elctp12kin.netlify.app';
+ 
+    $mail->CharSet = 'UTF-8';
+ 
+    $mail->FromName = '第三屆澳門高中生手機網頁設計技能大賽作品提交';
+ 
+    $mail->From = 'elctp12kin.netlify.app';
+ 
+    $mail->isHTML(true); 
+ 
+    $mail->addAddress($to,'lileekath456@gmail.com');
+  
+    $mail->Subject = $title;
+ 
+    $mail->Body = $content;
+ 
+    $mail->addAttachment($img);
+ 
+    $status = $mail->send();
+ 
+    if($status) {
+        return true;
+    }else{
+        return false;
+    }
+}
+ 
+$msgname = $_POST["發現中小企"];
+$msgcontent = $_POST["發現中小企"];
+$content = "姓名：{$nametextbox}".".
+            "所發現的企業：{$onetextbox}".
+            "對企業的意見:{$comments}";
+?>
